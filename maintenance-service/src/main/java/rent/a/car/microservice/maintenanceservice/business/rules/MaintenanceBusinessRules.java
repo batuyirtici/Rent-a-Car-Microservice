95a355166410a84a2125c42d7b6acce8d6986c71
Batuhan YIRTICI <85520902+batuyirtici@users.noreply.github.com>
2023-05-21 01:24:12 +0300

    added maintenance-service transactions

--- maintenance-service/src/main/java/rent/a/car/microservice/maintenanceservice/business/rules/MaintenanceBusinessRules.java
+++ maintenance-service/src/main/java/rent/a/car/microservice/maintenanceservice/business/rules/MaintenanceBusinessRules.java
@@ -14,29 +14,21 @@ public class MaintenanceBusinessRules {
     private final MaintenanceRepository repository;
     private final CarClient carClient;
 
-    public void checkIfMaintenanceExists(UUID id) {
-        if (!repository.existsById(id)) {
-            throw new BusinessException("MAINTENANCE_NOT_EXISTS");
-        }
-    }
+    public void checkIfMaintenanceExists(UUID id)
+    { if (!repository.existsById(id)) throw new BusinessException("MAINTENANCE_NOT_EXISTS"); }
 
     public void checkIfCarUnderMaintenance(UUID carId) {
-        if (repository.existsByCarIdAndIsCompletedIsFalse(carId)) {
+        if (repository.existsByCarIdAndIsCompletedIsFalse(carId))
             throw new BusinessException("CAR_IS_CURRENTLY_UNDER_MAINTENANCE");
-        }
     }
 
     public void checkIfCarIsNotUnderMaintenance(UUID carId) {
-        if (!repository.existsByCarIdAndIsCompletedIsFalse(carId)) {
+        if (!repository.existsByCarIdAndIsCompletedIsFalse(carId))
             throw new BusinessException("CAR_NOT_REGISTERED_FOR_MAINTENANCE");
-        }
     }
 
     public void checkIfCarAvailable(UUID carId) {
         var response = carClient.checkIfCarAvailable(carId);
-        if (!response.isSuccess()) {
-            throw new BusinessException(response.getMessage());
-        }
+        if (!response.isSuccess()) throw new BusinessException(response.getMessage());
     }
-
 }
