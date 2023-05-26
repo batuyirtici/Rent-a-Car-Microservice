a34283cbd01b0a5a390586192e929ef4f957db97
Batuhan YIRTICI <85520902+batuyirtici@users.noreply.github.com>
2023-05-26 03:41:20 +0300

    added config-server

---
+++ config-server/src/test/java/rent/a/car/microservice/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package rent.a.car.microservice.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
